e30b516b897c4e0dafb890fb1451f44578efe4da
Aidor777 <guillaume.garin@live.com>
2023-02-08 11:07:15 +0100

    Use AWS Lambda to obtain a badge

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      badge: "",
+    };
+  }
+
+  componentDidMount() {
+    this.generateBadge(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries !== this.props.entries) {
+      this.generateBadge(this.props.entries);
+    }
+  }
+
+  generateBadge = (entries) => {
+    fetch(
+      `https://sd8cilapi8.execute-api.eu-west-3.amazonaws.com/rank?entryCount=${entries}`
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ badge: data.badge }))
+      .catch(console.error);
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f3">{`Rank badge: ${this.state.badge}`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
